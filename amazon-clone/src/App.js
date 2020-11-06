bc94e2b85b39de1c417369f2593034f8185a250f
Sophia Sagan <sophiabraddock82@gmail>
2020-11-05 20:20:24 -0500

    navbar completed

---
+++ amazon-clone/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react'
+import Header from './Header';
+
+function App() {
+  return (
+    //BEM convention for styling
+    <div className="app">
+      
+      <Header />
+      {/* Home */}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB_TDGHuNnp4VFSBmLpt5Qk_BGKpmC-bME",
+    authDomain: "clone-d613f.firebaseapp.com",
+    databaseURL: "https://clone-d613f.firebaseio.com",
+    projectId: "clone-d613f",
+    storageBucket: "clone-d613f.appspot.com",
+    messagingSenderId: "288991095367",
+    appId: "1:288991095367:web:1b1615a291c95315721f62",
+    measurementId: "G-0N76WJN5GE"
+  };
