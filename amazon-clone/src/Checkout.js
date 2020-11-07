3cc3b4079a69a9b6cc01c0c30de3d325ff9c724c
Sophia Sagan <sophiabraddock82@gmail>
2020-11-06 22:14:23 -0500

    remove from basket working

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./Checkout.css";
+import CheckoutProduct from "./CheckoutProduct";
+import { useStateValue } from "./StateProvider";
 import Subtotal from "./Subtotal";
-// import Subtotal from "./Subtotal";
+
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -16,11 +20,16 @@ function Checkout() {
         <div>
           <h2 className="checkout__title">Your shopping Basket</h2>
 
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
-          {/* BasketItem */}
+          {basket.map(item => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
+
         </div>
       </div>
 
--- amazon-clone/src/reducer.js
+++ amazon-clone/src/reducer.js
@@ -1,19 +1,42 @@
 export const initialState = {
-    basket: [],
-  }; // create initial state
-  
-  const reducer = (state, action) => {
-    console.log(action);
-    switch (action.type) {
-      case "ADD_TO_BASKET":
-        return {
-          ...state,
-          basket: [...state.basket, action.item],
-        };
-  
-      default:
-        return state;
-    }
-  };
-  
-  export default reducer;
+  basket: [],
+}; // create initial state
+
+// selector
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0); //iterates through the basket, tallies the total and returns it as value
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "ADD_TO_BASKET":
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+      let newBasket = [...state.basket]; // copy of current basket
+
+      if (index >= 0) {
+        newBasket.splice(index, 1); // cut out the element
+      } else {
+        console.warn(
+          `Cant remove product (id: ${action.id}) as its not in basket!`
+        );
+      }
+
+      return {
+        ...state,
+        basket: newBasket,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
