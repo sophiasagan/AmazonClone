95f75a9fe3162cbeda00292529d69d829c47da4b
Sophia Sagan <sophiabraddock82@gmail>
2020-11-06 19:57:30 -0500

    add to basket working

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Checkout.css";
+import Subtotal from "./Subtotal";
 // import Subtotal from "./Subtotal";
 
 function Checkout() {
@@ -24,7 +25,7 @@ function Checkout() {
       </div>
 
       <div className="checkout__right">
-        {/* <Subtotal /> */}
+        <Subtotal />
       </div>
     </div>
   );
