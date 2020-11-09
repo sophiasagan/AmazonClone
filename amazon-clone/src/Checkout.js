bdb57cc22daf5acb5fc918cf09dfa1e4e3e28f7d
Sophia Sagan <sophiabraddock82@gmail>
2020-11-09 17:02:32 -0500

    updates to checkout

--- amazon-clone/src/Checkout.js
+++ amazon-clone/src/Checkout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
 import { useStateValue } from "./StateProvider";
@@ -6,6 +7,22 @@ import Subtotal from "./Subtotal";
 
 function Checkout() {
   const [{ basket, user }, dispatch] = useStateValue();
+  const emptyBasket = () => {
+    return (
+      <div className="emptyBasket">
+        <img
+          src="https://m.media-amazon.com/images/G/01/cart/empty/kettle-desaturated._CB445243794_.svg"
+          alt="Empty Cart"
+        />
+        <div className="emptyBasket__container">
+          <p>Your Amazon Cart is empty</p>
+          <Link to="/">
+            <button className="emptyBasketBtn">Add some items</button>
+          </Link>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div className="checkout">
@@ -17,9 +34,12 @@ function Checkout() {
         />
 
         <div>
-          <h3>Hello, {user?.email}</h3>
+          <h3>
+            Hello,{" "}
+            {user ? user.email.substring(0, user.email.indexOf("@")) : "Guest"}
+          </h3>
           <h2 className="checkout__title">Your shopping Basket</h2>
-
+          {basket.length === 0 && emptyBasket()}
           {basket.map((item) => (
             <CheckoutProduct
               id={item.id}
