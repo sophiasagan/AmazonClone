c90fa0c4c58dc92482ee51de3c8b3350842fd540
Sophia Sagan <sophiabraddock82@gmail>
2020-11-09 17:12:10 -0500

    styling updates

--- amazon-clone/src/Orders.js
+++ amazon-clone/src/Orders.js
@@ -1,42 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import { db } from './firebase';
-import Order from './Order';
-import './Orders.css'
-import { useStateValue } from './StateProvider';
+import React, { useEffect, useState } from "react";
+import { db } from "./firebase";
+import Order from "./Order";
+import "./Orders.css";
+import { useStateValue } from "./StateProvider";
 
 function Orders() {
-    const [{ basket, user }, dispatch] = useStateValue();
-    const [orders, setOrders] = useState([]);
+  const [{ basket, user }, dispatch] = useStateValue();
+  const [orders, setOrders] = useState([]);
 
-    useEffect(() => {
-        if(user){
-        db
-        .collection('users')
+  const notSignedIn = () => {
+    return <h3>Please sign in to view your order history.</h3>;
+  };
+
+  useEffect(() => {
+    if (user) {
+      db.collection("users")
         .doc(user?.uid)
-        .collection('orders')
-        .orderBy('created', 'desc')
-        .onSnapshot(snapshot => (
-            setOrders(snapshot.docs.map(doc => ({
-                id: doc.id,
-                data: doc.data()
-            })))
-        ))
+        .collection("orders")
+        .orderBy("created", "desc")
+        .onSnapshot((snapshot) =>
+          setOrders(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+        );
     } else {
-        setOrders([])
+      setOrders([]);
     }
-    }, [])
+  }, [user]);
 
-    return (
-        <div className='orders'>
-            <h1>Your orders</h1>
+  return (
+    <div className="orders">
+      <h1>Your orders</h1>
 
-            <div className='orders__order'>
-                {orders?.map(order => (
-                    <Order order={order} />
-                ))}
-            </div>
-        </div>
-    )
+      <div className="orders__order">
+        {!user && notSignedIn()}
+        {orders?.map((order) => (
+          <Order order={order} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Orders
+export default Orders;
